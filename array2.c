6641bbae1ec4e1c3d7173fbce0025442d279d7b5
tempCross <bt.will777@gmail.com>
2017-05-23 17:11:49 +0000

    Create array2.c

---
+++ array2.c
@@ -0,0 +1,24 @@
+# c_programmer
+exercise 1.6 (working)
+#include <stdio.h>
+int main()
+{
+        int c, nc, i, j;
+        int word[10];
+        nc = i = j = 0;
+        for(i = 0; i < 10; i++)
+          word[i] = 0;
+        while ((c = getchar()) != EOF){
+          if(c == ' '){
+            putchar('b');
+            j++;
+            nc = 0;
+          }
+          else {
+             nc++;
+          }   
+          for( i = 0; i < nc; i++)
+                word[j] = nc;
+}
+          printf("\n%d\n",word[1]);
+}
