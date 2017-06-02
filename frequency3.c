c6f84c00d014b112be5c3beb8fe890281fc2a47b
tempCross <bt.will777@gmail.com>
2017-06-02 02:21:01 -0400

    Create frequency3.c
    extended from answers..print histogram with stars

---
+++ frequency3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define NUM_CHARS 128
+
+int main()
+{
+    int c, i, j;
+    int char_freq[NUM_CHARS + 1];
+    
+    for( i = 0; i <= NUM_CHARS; i++ )
+      char_freq[i] = 0;
+      
+    while((c = getchar()) != EOF)
+      ++char_freq[c];
+    
+    for(i = 0; i <= NUM_CHARS; i++){
+      if(char_freq[i] > 0){
+        printf("\n%4d: ", i);
+        for(j = 0; j < char_feq[i]; j++)
+          printf("*");
+        printf("\n");
+      }  
+    }  
+}
